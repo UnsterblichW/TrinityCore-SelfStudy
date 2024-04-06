19fadc74e1656297d96df44d460fd3a16c673f91
SHADOW <unsterblich.wang@gmail.com>
2024-04-06 13:36:17 +0800

    修改：     netframe/server/EchoSession.cpp

--- netframe/server/EchoSession.cpp
+++ netframe/server/EchoSession.cpp
@@ -12,50 +12,50 @@ EchoSession::EchoSession(tcp::socket && socket) : Socket(std::move(socket))
 
 void EchoSession::Start()
 {
-    std::string ip_address = GetRemoteIpAddress().to_string();
-    std::cout << "Accepted connection from " << ip_address << std::endl;
+	std::string ip_address = GetRemoteIpAddress().to_string();
+	std::cout << "Accepted connection from " << ip_address << std::endl;
 
-    AsyncRead();
+	AsyncRead();
 }
 
 bool EchoSession::Update()
 {
-    return EchoSocket::Update();
+	return EchoSocket::Update();
 }
 
 void EchoSession::OnClose()
 {
-    std::string ip_address = GetRemoteIpAddress().to_string();
-    std::cout << "Closed connection at " << ip_address << std::endl;
-    EchoSocket::OnClose();
+	std::string ip_address = GetRemoteIpAddress().to_string();
+	std::cout << "Closed connection at " << ip_address << std::endl;
+	EchoSocket::OnClose();
 }
 
 void EchoSession::ReadHandler()
 {
-    MessageBuffer& packet = GetReadBuffer();
-    // 分隔完整的数据包
-    const char *rptr = reinterpret_cast<const char*>(packet.GetReadPointer());
-    size_t len = packet.GetActiveSize();
+	MessageBuffer& packet = GetReadBuffer();
+	// 分隔完整的数据包
+	const char *rptr = reinterpret_cast<const char*>(packet.GetReadPointer());
+	size_t len = packet.GetActiveSize();
 
-    std::string_view buf(rptr, len);
+	std::string_view buf(rptr, len);
 
-    // 因为模拟客户端发数据会用到telnet，而telnet的协议中将会用\r\n来当作每个数据包的分隔符，所以这里才这样写
-    size_t pos = buf.find_first_of("\r\n");
-    if (pos == std::string_view::npos)
-        return;
-    std::cout << "Recv from Client:" << std::string(rptr, pos+2);
+	// 因为模拟客户端发数据会用到telnet，而telnet的协议中将会用\r\n来当作每个数据包的分隔符，所以这里才这样写
+	size_t pos = buf.find_first_of("\r\n");
+	if (pos == std::string_view::npos)
+		return;
+	std::cout << "Recv from Client:" << std::string(rptr, pos + 2);
 
-    SendPacket(packet.GetReadPointer(), pos);
+	SendPacket(packet.GetReadPointer(), pos);
 
-    packet.ReadCompleted(pos+2);
-    AsyncRead();
+	packet.ReadCompleted(pos + 2);
+	AsyncRead();
 }
 
 void EchoSession::SendPacket(const uint8_t* msg, std::size_t len)
 {
-    MessageBuffer buf(len+4);
-    buf.Write("Server echo:", 13);
-    buf.Write(msg, len);
-    buf.Write("\r\n", 2);
-    QueuePacket(std::move(buf));
+	MessageBuffer buf(len + 13 + 2);
+	buf.Write("Server echo:", 13);
+	buf.Write(msg, len);
+	buf.Write("\r\n", 2);
+	QueuePacket(std::move(buf));
 }
