6b2964c0827da8ab95530bff23d7023cd255c794
SHADOW <unsterblich.wang@gmail.com>
2024-04-06 12:07:24 +0800

    调试配置

--- netframe/server/EchoSession.cpp
+++ netframe/server/EchoSession.cpp
@@ -39,10 +39,11 @@ void EchoSession::ReadHandler()
 
     std::string_view buf(rptr, len);
 
+    // 因为模拟客户端发数据会用到telnet，而telnet的协议中将会用\r\n来当作每个数据包的分隔符，所以这里才这样写
     size_t pos = buf.find_first_of("\r\n");
     if (pos == std::string_view::npos)
         return;
-    std::cout << "C:" << std::string(rptr, pos+2);
+    std::cout << "Recv from Client:" << std::string(rptr, pos+2);
 
     SendPacket(packet.GetReadPointer(), pos);
 
@@ -53,7 +54,7 @@ void EchoSession::ReadHandler()
 void EchoSession::SendPacket(const uint8_t* msg, std::size_t len)
 {
     MessageBuffer buf(len+4);
-    buf.Write("S:", 2);
+    buf.Write("Server echo:", 13);
     buf.Write(msg, len);
     buf.Write("\r\n", 2);
     QueuePacket(std::move(buf));
